02f402d67981da402998998ee57743c3d13f6555
Abel Pacheco <abelpacheco@outlook.com>
2015-10-05 02:03:52 -0500

    Fix default boolean in migration

--- db/migrate/20151005033733_add_is_admin_to_users.rb
+++ db/migrate/20151005033733_add_is_admin_to_users.rb
@@ -1,5 +1,5 @@
 class AddIsAdminToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :is_admin, :boolean, :default => 0
+    add_column :users, :is_admin, :boolean, :default => false
   end
 end
