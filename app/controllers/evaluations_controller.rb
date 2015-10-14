257d9ab688a83852c3f4599ef087b5896d4df76a
Abel Pacheco <abelpacheco@outlook.com>
2015-10-14 00:13:41 -0500

    Show required data in evaluations#index view

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -4,24 +4,15 @@ class EvaluationsController < ApplicationController
   	@functionary = @agency.functionary
   	@questions = Question.order("created_at")
 
-  	@agency_methods = %w[agency_target agency_programs agency_contact]
-  	@agency_labels  = ["Objetivos/Funciones de la dependencia", "Proyectos/Programas de la dependencia", "Información de contacto de la dependencia"]
-
-  	@functionary_methods = %w[functionary_functions functionary_schooling functionary_experience functionary_salary functionary_contact]
-  	@functionary_labels  = ["Metas del funcionario", "Perfil académico del funcionario", "Experiencia del funcionario", "Salario del funcionario", "Info. de contacto del funcionario"]
-  	
-  	@agency_hash = {
-  		"agency_target" => ["Objetivos/Funciones de la dependencia", "target"], 
-  		"agency_programs" => ["Proyectos/Programas de la dependencia", "programs"],
-  		"agency_contact" => ["Información de contacto de la dependencia", "contact"]
-  	}
-  	@functionary_hash = {
-  		"functionary_functions" => ["Metas del funcionario", "functions"],
-  		"functionary_schooling" => ["Perfil académico del funcionario", "schooling"],
-  		"functionary_experience" => ["Experiencia del funcionario", "experience"],
-  		"functionary_salary" => ["Salario del funcionario", "salary"],
-  		"functionary_contact" => ["Info. de contacto del funcionario", "contact"]
-  	}
-    # @hash = @agency_hash + @functionary_hash
+    @hash = {
+      "agency_target"          => ["Objetivos/Funciones de la dependencia",     "@agency", "target"], 
+      "agency_programs"        => ["Proyectos/Programas de la dependencia",     "@agency", "programs"],
+      "agency_contact"         => ["Información de contacto de la dependencia", "@agency", "contact"],
+      "functionary_functions"  => ["Metas del funcionario",                     "@functionary", "functions"],
+      "functionary_schooling"  => ["Perfil académico del funcionario",          "@functionary", "schooling"],
+      "functionary_experience" => ["Experiencia del funcionario",               "@functionary", "experience"],
+      "functionary_salary"     => ["Salario del funcionario",                   "@functionary", "salary"],
+      "functionary_contact"    => ["Info. de contacto del funcionario",         "@functionary", "contact"]
+    }
   end
 end
